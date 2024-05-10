e7c0abd0a17413fe2bc2f2dafb00ada085495720
Goutham-bhat <gouthambhat23@gmail.com>
2024-05-10 17:35:06 +0530

    commit 2

--- corejava_B92/src/com/tnsif/dayone/sample.java
+++ corejava_B92/src/com/tnsif/dayone/sample.java
@@ -5,6 +5,7 @@ public class sample {
 	public static void main(String[] args) {
 		
 		System.out.print("hello world");
+		System.out.println("welcome to github");
 		// TODO Auto-generated method stub
 
 	}
