eef9feca77cc85c31e671cc140ba9ae2f7142213
Goutham-bhat <gouthambhat23@gmail.com>
2024-05-10 17:24:40 +0530

    commit-1

---
+++ corejava_B92/src/com/tnsif/dayone/sample.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayone;
+
+public class sample {
+
+	public static void main(String[] args) {
+		
+		System.out.print("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
